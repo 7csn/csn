19c87a9ba4c3d23ab2dd22242560149ca8950c89
7csn <1479672753@qq.com>
2018-03-08 18:59:21 +0800

    WeChat payment settings

---
+++ csn/s/wx.php
@@ -0,0 +1,15 @@
+<?php
+
+//微信相关配置
+return [
+    // 微信支付APPID
+    'APPID' => '',
+    // 微信支付MCHID 商户收款账号
+    'MCHID' => '',
+    // 微信支付KEY
+    'KEY' => '',
+    // 公众帐号secert
+    'APPSECRET' => '',
+    // 接收支付状态的连接
+    'NOTIFY_URL' => '',
+];
