53fcd92a3d70f7c5e45d46398c9adeb75109e58b
7csn <1479672753@qq.com>
2018-03-08 17:27:29 +0800

    Project master configuration file

---
+++ csn/s/web.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    // 调试模式
+    'debug' => false,
+    // 伪静态
+    'rewrite' => true,
+    // 日期时区
+    'timezone' => 'PRC',
+    // 编码格式
+    'charset' => 'UTF-8',
+    // 路由分隔符
+    'separator' => '/',
+    // 模板编译
+    'template' => [
+        // 编译目录
+        'compile' => 'compile',
+        // 静态页目录
+        'html' => 'html',
+        // 静态页缓存秒数(0表示不缓存)
+        'cache_time' => 0
+    ],
+    // 开发者邮箱
+    'email' => '1479672753@qq.com'
+];
