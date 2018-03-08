d9f19057040d55dbb2df3ad512882bfd8937f172
7csn <1479672753@qq.com>
2018-03-08 22:27:26 +0800

    Routing switch object

---
+++ csn/t/Tap.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace csn\t;
+
+class Tap
+{
+
+    protected $method;      // 路由方法
+    protected $path;        // 路由标记
+    protected $where = [];  // 参数正则
+    protected $input = [];  // POST验证
+
+    // 开关对象
+    function __construct($method, $path)
+    {
+        $this->method = $method;
+        $this->path = $path;
+    }
+
+    // 路由正则条件
+    function where($key, $preg = null)
+    {
+        $where = is_array($key) ? $key : [$key => $preg];
+        foreach ($where as $key => $preg) {
+            $this->where[$key] = $preg;
+        }
+        foreach ($this->method as $method) {
+            Route::$tap[$method][$this->path]['where'] = $this->where;
+        }
+        return $this;
+    }
+
+    // POST正则条件
+    function input($key, $preg = null)
+    {
+        if (in_array('POST', $this->method)) {
+            $input = is_array($key) ? $key : [$key => $preg];
+            foreach ($input as $key => $preg) {
+                $this->input[$key] = $preg;
+            }
+            Route::$tap['POST'][$this->path]['input'] = $this->input;
+        }
+        return $this;
+    }
+
+    // 路由正则条件解析
+    function parse()
+    {
+        $path = str_replace('/', '\\/', $this->path);
+        foreach ($this->where as $key => $preg) {
+            $path = str_replace(['{' . $key . '}', '@{' . $key . '?}'], ['(' . $preg . ')', '(@' . $preg . ')?'], $path);
+        }
+        foreach ($this->method as $method) {
+            Route::$tap[$method][$this->path]['preg'] = $path;
+            Route::$tap[$method][$this->path]['parse'] = true;
+        }
+        return $path;
+    }
+
+}
