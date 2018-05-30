0d6cc547891195d0e9f0512db51ac2f573b39a53
7csn <1479672753@qq.com>
2018-05-30 15:44:03 +0800

    5.30:15 something about orm

--- csn/t/Model.php
+++ csn/t/Model.php
@@ -5,9 +5,9 @@ namespace csn\t;
 class Model
 {
 
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
     //  随机主从数据库地址
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
 
     // 获取写数据地址库组
     protected static function writes()
@@ -37,9 +37,9 @@ class Model
         return is_null(DbInfo::$slave) ? DbInfo::$slave = MS::rand(self::reads()) : DbInfo::$slave;
     }
 
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
     //  数据库连接
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
 
     // 获取数据库相关信息
     protected static function node($address)
@@ -59,7 +59,7 @@ class Model
             list($host, $port) = explode(':', $address);
             try {
                 $node = self::node($address);
-                $link = new \PDO("mysql:host=$host", $node['du'], $node['dp']);
+                $link = new \PDO("mysql:host=$host;port=$port", $node['du'], $node['dp']);
                 DbInfo::$dbInfos[$address] = ['dbn' => $node['dbn'], 'dbn_now' => null];
             } catch (\PDOException $e) {
                 Exp::end('[PDO]：' . str_replace("\n", '', iconv("GB2312// IGNORE", "UTF-8", $e->getMessage())));
@@ -68,9 +68,9 @@ class Model
         })();
     }
 
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
     //  表SQL封装
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
 
     // 查询
     protected static function query($func, $rArr = false)
@@ -106,12 +106,13 @@ class Model
         return is_array($sqls) ? $link->prepare($sqls[0])->execute($sqls[1]) : $link->exec($sqls);
     }
 
-    // ------------------------------------------
-    //  类操作
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
+    //  库、表、字段信息
+    // ----------------------------------------------------------------------
 
-    static $dbn = false;                // 当前库名
-    static $tbn = false;                // 当前表名
+    protected static $strict = false;      // 字段严格模式
+    protected static $tbn = false;         // 当前表名
+    protected static $dbn = false;         // 当前库名
 
     // 数据库匹配;返回连接
     protected static function dbnConnect($address)
@@ -143,14 +144,14 @@ class Model
     protected static function names()
     {
         $class = static::class;
-        $class::$tbn || (function($class) {
+        $class::$tbn || (function ($class) {
             if (strpos($class, 'app\\m\\') !== 0) {
-                Exp::end('数据库模型'.$class.'异常');
+                Exp::end('数据库模型' . $class . '异常');
             } else {
                 $arr = array_reverse(explode('\\', substr($class, 6)));
                 $count = count($arr);
                 if ($count === 0) {
-                    Exp::end('数据库模型'.$class.'异常');
+                    Exp::end('数据库模型' . $class . '异常');
                 } else {
                     $class::$tbn = $arr[0];
                     $class::$dbn = $arr[1] ?? false;
@@ -166,17 +167,17 @@ class Model
         return self::names()::$tbn;
     }
 
-    // 获取库名
-    protected static function dbn()
+    // 获取/设置库名
+    static function dbn($dbn = null)
     {
-        return self::names()::$dbn;
+        return is_null($dbn) ? self::names()::$dbn : self::names()::$dbn = $dbn;
     }
 
     // 查询表结构
     protected static function desc()
     {
-        $tbn = self::tbn();
-        return key_exists($tbn, DbInfo::$descs) ? DbInfo::$descs[$tbn] : DbInfo::$descs[$tbn] = (function () {
+        $key = self::dbn() . '@' . self::tbn();
+        return key_exists($key, DbInfo::$descs) ? DbInfo::$descs[$key] : DbInfo::$descs[$key] = (function () {
             $desc = new \stdClass();
             $desc->list = new \stdClass();
             foreach (self::query(function ($tbn) {
@@ -192,6 +193,47 @@ class Model
             return $desc;
         })();
     }
+
+    // 查询字段结构
+    protected static function fieldStructure($field)
+    {
+        $desc = self::desc();
+        return key_exists($field, $desc) ? $desc[$field] : null;
+    }
+
+    // 字段值处理
+    protected static function parseValue($structure, $val = null)
+    {
+        switch (explode('(', $structure->Type)[0]) {
+            case 'char':
+            case 'varchar':
+            case 'tinytext':
+            case 'longtext':
+            case 'mediumtext':
+            case 'text':
+                is_null($val) ? '' : (string)$val;
+                break;
+            case 'int':
+                is_null($val) ? 0 : floatval(intval);
+                break;
+            default:
+                is_null($val) ? 0 : floatval($val);
+                break;
+        }
+        return (!$val && !is_null($structure->Default)) ? $structure->Default : $val;
+    }
+
+    // 主键及对象锁定
+    protected static function primaryKey($id)
+    {
+        $desc = self::desc();
+        $primaryKey = $desc->primaryKey;
+        return is_null($primaryKey) ? Exp::end((self::dbn() ? '库' . self::dbn() : '默认库') . '中表' . self::tbn() . '主键不存在') : [$primaryKey, self::parseValue($desc->list->$primaryKey, $id)];
+    }
+
+    // ----------------------------------------------------------------------
+    //  表常规操作(类)
+    // ----------------------------------------------------------------------
 
     // 重置表结构
     static function truncate()
@@ -210,21 +252,105 @@ class Model
         }, $rArr);
     }
 
-    // 条件
-    static function where($where)
+    // 删除指定行
+    static function destroy($id)
     {
+        list($primaryKey, $id) = self::primaryKey($id);
+        return self::execute(function ($tbn) use ($primaryKey, $id) {
+            return [" DELETE FROM `$tbn` WHERE `$primaryKey` = :id ", [':id' => $id]];
+        });
     }
 
-    // ------------------------------------------
-    //  确定对象
-    // ------------------------------------------
+    // ----------------------------------------------------------------------
+    //  静态指定对象
+    // ----------------------------------------------------------------------
 
+    // 条件
+    static function which($where, $bind = null, $obj = null)
+    {
+        (is_null($obj) || !($obj instanceof self)) && $obj =  new self();
+        return $obj->where($where)->bind($bind);
+    }
 
-    // ------------------------------------------
-    //  对象操作
-    // ------------------------------------------
+    static function find($id)
+    {
+        list($primaryKey, $id) = self::primaryKey($id);
+        return self::which(" `$primaryKey` = :id ", [':id' => $id]);
+    }
 
-    protected $data = [];           // 对象属性
+    // ----------------------------------------------------------------------
+    //  对象配置
+    // ----------------------------------------------------------------------
+
+    protected $data = [];                   // 对象属性
+
+    // 创建对象
+    function __construct($where = null, $bind = null)
+    {
+        $this->parse()->where($where)->bind($bind);
+    }
+
+    // 指定条件对象
+    private function parse()
+    {
+        $this->parse = new \stdClass();
+        $this->parse->data = [];
+        $this->parse->__get = function($key) {
+            return key_exists($key, $this->data) ? $this->data[$key] : null;
+        };
+        $this->parse->__set = function($key, $value) {
+            $this->data[$key] = $value;
+        };
+        return $this;
+    }
+
+    // 条件
+    function where($where, $bind = null)
+    {
+        empty($where) || ($this->bind($bind)->parse->where = $where);
+        return $this;
+    }
+
+    // 预编译
+    function bind($bind)
+    {
+        empty($bind) || (function($obj, $bind) {
+            $obj->parse->bind = is_null($b = $obj->parse->bind) ? $bind : array_merge($b, $bind);
+        })($this, $bind);
+        return $this;
+    }
+
+    // 字段
+    function field($field, $bind = null)
+    {
+        empty($field) || ($this->bind($bind)->parse->field = $field);
+        return $this;
+    }
+
+    // 归类
+    function group($group)
+    {
+        $this->parse->group = $group;
+        return $this;
+    }
+
+    // 顺序
+    function order($order)
+    {
+        $this->parse->order = $order;
+        return $this;
+    }
+
+    // 限制
+    function limit($limit)
+    {
+        $this->parse->limit = $limit;
+        return $this;
+    }
+
+    // ----------------------------------------------------------------------
+    //  表常规操作(对象)
+    // ----------------------------------------------------------------------
 
     // 设置字段值
     function __set($key, $value)
@@ -237,18 +363,5 @@ class Model
     {
         return key_exists($key, $this->data) ? $this->data[$key] : null;
     }
-
-
-    static function test()
-    {
-        return substr(strrchr(static::class, '\\'), 1);
-    }
-
-//
-//    // 查询
-//    static function find()
-//    {
-//        return self::name();
-//    }
 
 }
